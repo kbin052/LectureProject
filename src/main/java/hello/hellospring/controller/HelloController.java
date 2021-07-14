cb2325bb4d2b529b3a106cbe277b5e395ee75f75
kbin052 <70202358+kbin052@users.noreply.github.com>
2021-07-14 15:51:39 +0900

    7강 Update

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +20,29 @@ public class HelloController {
     {
         model.addAttribute("name",name);
         return "Hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http에서 body부에 아래 내용을 직접 넣어주겠다는 의미
+    public String helloString(@RequestParam("name")String name){
+        return "hello" + name; //"hello spring"
+    }
+
+    @GetMapping("Hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
     }
 }
